b16d0d294cf4b7b01cd0af795c511cbf1dec81c7
hongdonghyun <hong4367@gmail.com>
2019-10-16 20:56:04 +0900

    자판기

---
+++ Day3/Day3/Day3/RedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RedViewController.swift
+//  Day3
+//
+//  Created by Hongdonghyun on 16/10/2019.
+//  Copyright © 2019 hongdonghyun. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Day3/Day3/Day3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Day3
+//
+//  Created by Hongdonghyun on 16/10/2019.
+//  Copyright © 2019 hongdonghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var menuImages: [UIImageView]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+}
+
+    @IBAction func increaseButton(_ sender: Any) {
+    }
+}
